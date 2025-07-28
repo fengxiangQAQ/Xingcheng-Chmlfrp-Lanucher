665a0291ae1713bf6bbbd9e11633925e9e973407
枫相 <140891522+FengXiang2233@users.noreply.github.com>
2025-07-28 23:03:57 +0800

    构建工作流修补

--- core/object/gui/page/Mask.py
+++ core/object/gui/page/Mask.py
@@ -8,7 +8,6 @@ class MaskFrame(ctk.CTkFrame):
         super().__init__(master,width=784,height=418,corner_radius=0,fg_color="#0000ff")
     
     def update_mask(self):
-        print(1)
         self.mask=not self.mask
         if self.mask:
             self.configure(fg_color="#000000")
